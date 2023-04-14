803bf1feafb232a3d642b71c01c28da3e4c4b9c1
zroes <zk.roes@gmail.com>
2023-04-14 10:28:57 -0600

    location button on account

--- GroceryTrip.client/src/pages/AccountPage.vue
+++ GroceryTrip.client/src/pages/AccountPage.vue
@@ -1,18 +1,33 @@
 <template>
-  <div class="about text-center">
+  <div class="about text-center text-light">
     <h1>Welcome {{ account.name }}</h1>
     <img class="rounded" :src="account.picture" alt="" />
     <p>{{ account.email }}</p>
+    <button @click="promptLocation" class="btn btn-primary">Get my Location</button>
   </div>
 </template>
 
 <script>
 import { computed } from 'vue'
 import { AppState } from '../AppState'
+import { logger } from "../utils/Logger.js"
+import Pop from "../utils/Pop.js"
 export default {
   setup() {
     return {
-      account: computed(() => AppState.account)
+      account: computed(() => AppState.account),
+      async promptLocation() {
+        try {
+          function success(pos) {
+            const crd = pos.coords
+            logger.log(crd.latitude, crd.longitude)
+          }
+          navigator.geolocation.getCurrentPosition(success)
+        } catch (error) {
+          logger.error(error)
+          Pop.error(error.message)
+        }
+      }
     }
   }
 }
