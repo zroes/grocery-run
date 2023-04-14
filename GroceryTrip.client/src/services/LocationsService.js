fd14a435bd7d0fb4cff96aa25e858b20df6d8384
zroes <zk.roes@gmail.com>
2023-04-14 14:05:54 -0600

    adding imports

--- GroceryTrip.client/src/services/LocationsService.js
+++ GroceryTrip.client/src/services/LocationsService.js
@@ -1,10 +1,12 @@
 import { AppState } from "../AppState.js"
 import { logger } from "../utils/Logger.js"
+import { api } from "./AxiosService.js"
+import { StoreLocation } from "../models/StoreLocation.js"
 
 class LocationsService {
   async sendLatLong(coords) {
     logger.log(coords)
-    const res = await api.post('/location', coords)
+    const res = await api.post('api/location', coords)
     AppState.locations = res.data.map(l => new StoreLocation(l))
     logger.log(AppState.locations)
   }
