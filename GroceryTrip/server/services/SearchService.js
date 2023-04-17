327a64f058286ad5809ea1753e2cf46b879636e2
josh <joshjj3@gmail.com>
2023-04-17 11:55:09 -0600

    search working for one item

--- GroceryTrip/server/services/SearchService.js
+++ GroceryTrip/server/services/SearchService.js
@@ -22,21 +22,47 @@ class SearchService {
       }
     })
     // return res0.data
-    const parsedRes = JSON.parse(res0.data)
-    const mappedRes = parsedRes.data.map(item => new SearchItem(item))
-    mappedRes.forEach(item => item.locationId = locations[0].locationId)
-    return mappedRes
-    // resArray.push(mappedRes)  // r.data
+    const parsedRes0 = JSON.parse(res0.data)
+    for (let i = 0; i < parsedRes0.data.length; i++) {
+      let item = parsedRes0.data[i]
+      item.locationId = locations[0].locationId
+      item.store = "FRED MEYER"
+      resArray.push(new SearchItem(item))
+    }
+    // const mappedRes0 = parsedRes0.data.map(item => new SearchItem(item))
+    // mappedRes0.forEach(item => item.locationId = locations[0].locationId)
+    // return mappedRes0
 
-    // const res1 = await Kroger.get('products', {
-    //   headers: { 'Authorization': `Bearer ${token}` },
-    //   params: {
-    //     'filter.locationId': locations[1],
-    //     // 'filter.term': q
-    //     'filter.term': query,
-    //     // 'filter.limit': '1'
-    //   }
-    // })
+    // FIXME dont forget to uncooment
+    const res1 = await Kroger.get('products', {
+      headers: { 'Authorization': `Bearer ${token}` },
+      params: {
+        'filter.locationId': locations[1].locationId,
+        // 'filter.term': q
+        'filter.term': query,
+        // 'filter.limit': '1'
+      }
+    })
+
+    const parsedRes1 = JSON.parse(res1.data)
+    for (let i = 0; i < parsedRes1.data.length; i++) {
+      let item = parsedRes0.data[i]
+      item.locationId = locations[1].locationId
+      item.store = "FRED MEYER"
+      resArray.push(new SearchItem(item))
+    }
+
+    return resArray
+
+
+    // const mappedRes1 = parsedRes1.data.map(item => new SearchItem(item))
+    // mappedRes1.forEach(item => item.locationId = locations[1].locationId)
+    // resArray.push(mappedRes1)
+    // return resArray.flat(1)
+    // FIXME the end of comment to be added
+
+
+
 
     // res1.data.forEach(item => item.locationId = locations[1].locationId)
 
