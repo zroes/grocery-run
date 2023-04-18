6bdd44dae2a7959ec4774068830e1fe28766614d
zroes <zk.roes@gmail.com>
2023-04-18 11:41:16 -0600

    get trip items

--- GroceryTrip.client/src/components/MyTrip.vue
+++ GroceryTrip.client/src/components/MyTrip.vue
@@ -1,15 +1,28 @@
 <template>
-  <h1>My trips</h1>
+  <h4>My Trip</h4>
 </template>
 
 <script>
-import { computed } from "vue"
+import { computed, onMounted } from "vue"
 import { AppState } from "../AppState.js"
 import { logger } from "../utils/Logger.js"
 import Pop from "../utils/Pop.js"
+import { tripService } from "../services/TripService.js"
 export default {
   setup() {
     // private variables and methods here
+    async function getMyTripItems() {
+      try {
+        await tripService.getMyTripItems()
+      } catch (error) {
+        logger.error(error)
+        Pop.error(error.message)
+      }
+    }
+
+    onMounted(() => {
+      getMyTripItems()
+    })
     return {
       // public variables and methods here
     }
