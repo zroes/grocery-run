e6c715467a3f7be5d9b07d6d97e69ca34b1d23be
zroes <zk.roes@gmail.com>
2023-04-18 15:09:26 -0600

    sdomthing

---
+++ GroceryTrip.client/src/components/StoreTripCard.vue
@@ -0,0 +1,18 @@
+<template></template>
+
+<script>
+import { computed } from "vue"
+import { AppState } from "../AppState.js"
+import { logger } from "../utils/Logger.js"
+import Pop from "../utils/Pop.js"
+export default {
+  setup() {
+    // private variables and methods here
+    return {
+      // public variables and methods here
+    }
+  },
+}
+</script>
+
+<style></style>
