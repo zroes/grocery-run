747ec1700fc466a69a54344541dbbcb070ccf915
josh <joshjj3@gmail.com>
2023-07-06 22:22:35 -0600

    single search works

--- GroceryTrip/server/services/AxiosService.js
+++ GroceryTrip/server/services/AxiosService.js
@@ -5,7 +5,7 @@ const krogerURL = 'https://api-ce.kroger.com/v1/'
 export const Kroger = new Axios({
   baseURL: krogerURL,
   timeout: 8000,
-  headers: { 'Authorization': 'Basic Z3JvY2VyeXJ1bi03YjAzNTkyYWEzZTIyY2UwOWZlZDc2MWI4YWQ1MDBhNTczODYxMzU2ODEzNTYzMjc5MDc6dE0ydm5JN19CdzlwaXdvWnplcldNcWMtd1lpeDVjRnVjVk1aYXJGNw==', 'Content-Type': 'application/x-www-form-urlencoded' }
+  headers: { 'Authorization': 'Basic Z3JvY2VyeXJ1bi03YjAzNTkyYWEzZTIyY2UwOWZlZDc2MWI4YWQ1MDBhNTczODYxMzU2ODEzNTYzMjc5MDc6RHhMSVFsakJhTzR0SEg5d1ZWMWQ4VDRMRWE0bGxTOFk0Z3FucHVpNg==', 'Content-Type': 'application/x-www-form-urlencoded' }
 })
 
 
--- GroceryTrip/server/services/KrogerAuthorizationService.js
+++ GroceryTrip/server/services/KrogerAuthorizationService.js
@@ -2,14 +2,14 @@ import { Kroger } from "./AxiosService.js"
 
 
 class KrogerAuthorizationService {
-    async getAuthorization() {
-        const res = await Kroger.post('connect/oauth2/token?grant_type=client_credentials&scope=product.compact')
-        // krogerToken.token = res.data.access_token
-        // krogerToken.CreatedAt = Date.now()
-        const token = JSON.parse(res.data)
-        return token.access_token
-    }
-    // TODO refactor to cache token for 25 mins
+  async getAuthorization() {
+    const res = await Kroger.post('connect/oauth2/token?grant_type=client_credentials&scope=product.compact')
+    // krogerToken.token = res.data.access_token
+    // krogerToken.CreatedAt = Date.now()
+    const token = JSON.parse(res.data)
+    return token.access_token
+  }
+  // TODO refactor to cache token for 25 mins
 }
 
 
