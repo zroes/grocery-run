693bf86e41ab69b66b727b273efc018b5a7ebac7
zroes <zk.roess@gmail.com>
2023-07-08 22:32:28 -0600

    lists

--- GroceryTrip.client/src/env.js
+++ GroceryTrip.client/src/env.js
@@ -1,5 +1,5 @@
 export const dev = window.location.origin.includes('localhost')
-export const baseURL = dev ? 'http://localhost:3000' : ''
+export const baseURL = dev ? 'http://localhost:3000' : 'https://grocery-run.onrender.com'
 export const useSockets = false
 export const domain = 'dev-wrya5phstjdz8awa.us.auth0.com'
 export const clientId = 'AP7043rVAyIAaKzgd3JkL61hWnjf59QY'
